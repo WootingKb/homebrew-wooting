ee103bd1179b7096cdc0387abe54e91ca7f5ef88
simon-wh <simon@wooting.io>
2021-05-17 11:02:22 +0100

    Formula cleanup for CI

--- Formula/wooting-analog-sdk.rb
+++ Formula/wooting-analog-sdk.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class WootingAnalogSdk < Formula
   desc "Native support for Analog Keyboards #WootDev"
   homepage "https://github.com/WootingKb/wooting-analog-sdk"
@@ -9,17 +6,8 @@ class WootingAnalogSdk < Formula
   license "MPL-2.0"
   bottle :unneeded
   
-  # depends_on "cmake" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    #system "./configure", "--disable-debug",
-    #                      "--disable-dependency-tracking",
-    #                      "--disable-silent-rules",
-    #                      "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-    
     lib.install "wrapper/sdk/libwooting_analog_sdk.dylib"
     
     (share/"WootingAnalogPlugins").mkpath
@@ -31,15 +19,6 @@ class WootingAnalogSdk < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test wooting-analog-sdk`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    system "true"
   end
 end

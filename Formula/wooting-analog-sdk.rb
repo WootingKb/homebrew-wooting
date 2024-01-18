673cc3a5e5da6f11f56c64d0ca661ef8baffa88c
simon-wh <simon@wooting.io>
2024-01-18 13:55:50 +0100

    Try updating to latest v0.7.4 and supporting arm and intel builds

--- Formula/wooting-analog-sdk.rb
+++ Formula/wooting-analog-sdk.rb
@@ -1,8 +1,14 @@
 class WootingAnalogSdk < Formula
   desc "Native support for Analog Keyboards #WootDev"
   homepage "https://github.com/WootingKb/wooting-analog-sdk"
-  url "https://github.com/WootingKb/wooting-analog-sdk/releases/download/v0.7.0/wooting-analog-sdk-v0.7.0-x86_64-apple-darwin.tar.gz"
-  sha256 "11f9df829a13745a616d42a52e855eac785b15ec9edec66be3e8699cc3f7b812"
+  on_intel do
+    url "https://github.com/WootingKb/wooting-analog-sdk/releases/download/v0.7.4/wooting-analog-sdk-v0.7.4-x86_64-apple-darwin.tar.gz"
+    sha256 "f03b45b68681f0d47565536e0e44993db6923106fa07bfe34b8c7d672f6c0c1b"
+  end
+  on_arm do
+    url "https://github.com/WootingKb/wooting-analog-sdk/releases/download/v0.7.4/wooting-analog-sdk-v0.7.4-aarch64-apple-darwin.tar.gz"
+    sha256 "dadcf083b84276be87b84266323411c6668d35a55d1e2e566df4e17ff8c31b69"
+  end
   license "MPL-2.0"
 
   def install
